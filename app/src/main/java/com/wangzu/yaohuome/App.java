e40ed339d3a637b1d7778bad0b0c5be817c078a2
li1573335865 <30287547+li1573335865@users.noreply.github.com>
2017-12-07 17:55:22 +0800

    2017/12/7

---
+++ app/src/main/java/com/wangzu/yaohuome/App.java
@@ -0,0 +1,15 @@
+package com.wangzu.yaohuome;
+
+import android.app.Application;
+
+/**
+ * Created by AnJiuZhe on 2017/12/7 0007.
+ * Email 1573335865@qq.com
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
