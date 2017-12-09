d89802d5492e435873cfdf2b88fa9069b075ddf0
AnRuoXi <31235098+AnRuoXi@users.noreply.github.com>
2017-12-09 18:42:42 +0800

    2017、12、9

--- app/src/main/java/com/wangzu/yaohuome/api/Api.java
+++ app/src/main/java/com/wangzu/yaohuome/api/Api.java
@@ -12,7 +12,7 @@ public class Api {
     public final static String USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0";
     public final static String LOGIN_RRL = "waplogin.aspx";
 
-    public final static String HOST = "http://yaohw.com/";
+    public final static String HOST = "https://yaohw.com/";
     public final static String NEWPOSTURL = "bbs/book_list.aspx?action=new";
     public static Map<String,String> COOKIES = null;
 }
